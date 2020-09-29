71b8859ea81dbc9a5ec1702e02dc37932764dfb2
WONILLISM <wonillism@gmail.com>
2020-09-29 13:56:56 +0900

    init libft.h

---
+++ 01_Libft/libft.h
@@ -0,0 +1,10 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+size_t  ft_strlcpy(char *dst, const char *src, size_t size);
+
+#endif
