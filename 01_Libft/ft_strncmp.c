b900d6f7e6652b50aa73df50c5c14add072bcde1
WONILLISM <wonillism@gmail.com>
2020-09-29 18:47:50 +0900

    2020-09-29 ft_strncmp first commit

---
+++ 01_Libft/ft_strncmp.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return ((int)s1[i] - (int)s1[j]);
+		i++;
+	}
+	return (0);
+}
