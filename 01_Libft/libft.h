e81cc0a5babc18b411602b9b093e06d2afbb478b
WONILLISM <wonillism@gmail.com>
2020-09-29 16:15:59 +0900

    2020-09-29 add ft_strlcat ft_strchr

--- 01_Libft/libft.h
+++ 01_Libft/libft.h
@@ -11,5 +11,6 @@ char	*ft_strchr(const char *s, int c);
 
 size_t	ft_strlen(const char *s);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
