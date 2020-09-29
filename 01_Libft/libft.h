f0e4b923ede8aa6188c7def21641ea09a0d90568
WONILLISM <wonillism@gmail.com>
2020-09-29 16:12:40 +0900

    2020-09-29 add ft_lcpy

--- 01_Libft/libft.h
+++ 01_Libft/libft.h
@@ -4,6 +4,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strchr(const char *s, int c);
+
 size_t	ft_strlen(const char *s);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
 
