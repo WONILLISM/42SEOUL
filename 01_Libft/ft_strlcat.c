3106e30120900433fafaaf3779b1842bb1da249d
WONILLISM <wonillism@gmail.com>
2020-09-29 13:58:03 +0900

    2020-09-29 ft_strlcat first commit

--- 01_Libft/ft_strlcat.c
+++ 01_Libft/ft_strlcat.c
@@ -1,8 +1,20 @@
 #include "libft.h"
 
-size_t  strlcat(char * dst, const char * src, size_t size)
+size_t  strlcat(char *dst, const char *src, size_t size)
 {
-    size_t  dst_len;
-    size_t  dst_src;
-    
+	size_t  dst_len;
+	size_t  src_len;
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	while (j < size && dst[j])
+		j++;
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	while (i < src_len && dst_len + i + 1 < size)
+		dst[dst_len + i] = src[i++];
+	dst[dst_len + i] = '\0';
+	return (j + src_len);
 }
