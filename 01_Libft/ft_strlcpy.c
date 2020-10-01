182b920b11fba42845aefc28f14a62e2a8e48d81
WONILLISM <wonillism@gmail.com>
2020-10-01 13:46:13 +0900

    2020-10-01 compare strlcpy strncpy

--- 01_Libft/ft_strlcpy.c
+++ 01_Libft/ft_strlcpy.c
@@ -5,7 +5,7 @@ size_t  ft_strlcpy(char * dst, const char *src, size_t size)
 	size_t	src_len;
 	size_t	idx;
 
-	if (!*dst && !*src)
+	if (dst == NULL && src == NULL)
 		return (0);
 	src_len	=	ft_strlen(src);
 	idx		=	0;
@@ -17,4 +17,16 @@ size_t  ft_strlcpy(char * dst, const char *src, size_t size)
 	if (size > 0)
 		dst[idx] = '\0';
 	return (src_len);
+}
+
+
+char *ft_strncpy(char *dst, const char *src, size_t n)
+{
+	if ((dst == NULL && src == NULL) || !n)
+		return (0);
+    while ((n--) && *src)
+        *dst++ = *src++;
+    while (n--)
+        *dst++ = '\0';
+    return (dst);
 }
