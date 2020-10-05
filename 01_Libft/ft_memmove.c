c3b491ccc06213cc56d0a89aa1df5567cfe34741
Wonil Park <wopark@c6r5s4.42seoul.kr>
2020-10-05 20:52:41 +0900

    2020-10-05 norm checked

--- 01_Libft/ft_memmove.c
+++ 01_Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: wopark <wopark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 17:24:13 by wopark            #+#    #+#             */
-/*   Updated: 2020/10/05 17:24:14 by wopark           ###   ########.fr       */
+/*   Updated: 2020/10/05 20:51:11 by wopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	unsigned	char	tmp[n];
+
 	ft_memcpy(tmp, src, n);
 	ft_memcpy(dst, tmp, n);
 	return (dst);
