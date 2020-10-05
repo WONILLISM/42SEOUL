e48a5768a3caca5e68911dd84d5ef0b0c4ae6dbb
Wonil Park <wopark@c6r5s4.42seoul.kr>
2020-10-05 20:55:54 +0900

    2020-10-05 ft_strchr norm checked

--- 01_Libft/ft_strchr.c
+++ 01_Libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: wopark <wopark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 17:24:22 by wopark            #+#    #+#             */
-/*   Updated: 2020/10/05 17:24:24 by wopark           ###   ########.fr       */
+/*   Updated: 2020/10/05 20:54:41 by wopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ char	*ft_strchr(const char *s, int c)
 {
 	if (s == NULL)
 		return (0);
-
 	while (*s)
 	{
 		if (*s == (char)c)
