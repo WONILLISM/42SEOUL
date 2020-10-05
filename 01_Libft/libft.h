2fc8a53f0acf3a8296b6a0443fa40b3e04c002da
Wonil Park <wopark@c6r5s4.42seoul.kr>
2020-10-05 22:18:48 +0900

    2020-10-05 end

--- 01_Libft/libft.h
+++ 01_Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: wopark <wopark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 17:18:39 by wopark            #+#    #+#             */
-/*   Updated: 2020/10/05 20:05:25 by wopark           ###   ########.fr       */
+/*   Updated: 2020/10/05 21:22:29 by wopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 #include <stdio.h>
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		memcmp(const void *s1, const void *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
@@ -43,7 +43,7 @@ long	ft_atoi(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-char	*ft_strdup(const char *s);
+char	*ft_strdup(char *src);
 
 size_t	ft_strlen(const char *s);
 size_t  ft_strlcpy(char *dst, const char *src, size_t size);
