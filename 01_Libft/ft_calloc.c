9d8eea39c89ec57fd6948b0f9f3c2a5c6de1fd33
Wonil Park <wopark@c6r5s4.42seoul.kr>
2020-10-05 17:03:26 +0900

    2020-10-05 ft_calloc first commit

---
+++ 01_Libft/ft_calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wopark <wopark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/05 16:53:06 by wopark            #+#    #+#             */
+/*   Updated: 2020/10/05 16:58:55 by wopark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ret;
+
+	if (!(ret = (void *)malloc(count * size)))
+		return (0);
+	ft_bzero(ret, count * size);
+	return (ret);
+}
