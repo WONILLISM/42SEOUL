1a86e74ebd73191c7e1dc93c7373d62b21bf7ed7
Wonil Park <wopark@c1r5s4.42seoul.kr>
2020-10-09 14:37:49 +0900

    test

--- 01_Libft/ft_strlcpy.c
+++ 01_Libft/ft_strlcpy.c
@@ -6,35 +6,35 @@
 /*   By: wopark <wopark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 17:25:18 by wopark            #+#    #+#             */
-/*   Updated: 2020/10/09 01:16:06 by wopark           ###   ########.fr       */
+/*   Updated: 2020/10/09 14:37:24 by wopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	const char	*s;
 	size_t		n;
 
 	s = src;
 	n = size;
-	if (!dst && !src)
+	if (!dest && !src)
 		return (0);
 	if (n != 0)
 	{
 		while (--n != 0)
 		{
-			if ((*dst = *s) == '\0')
+			if ((*dest = *s) == '\0')
 				break ;
-			dst++;
+			dest++;
 			s++;
 		}
 	}
 	if (n == 0)
 	{
 		if (size != 0)
-			*dst = '\0';
+			*dest = '\0';
 		while (*s)
 			s++;
 	}
