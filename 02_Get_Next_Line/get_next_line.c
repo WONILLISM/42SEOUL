6aeb9144091682be9d65b3beae886d2dc6ec8263
Wonil Park <wopark@c5r3s3.42seoul.kr>
2020-10-16 16:16:52 +0900

    2020-10-16 reset code

--- 02_Get_Next_Line/get_next_line.c
+++ 02_Get_Next_Line/get_next_line.c
@@ -2,29 +2,6 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-static int		ret_line(char **line, char *store, char *tmp_char)
-{
-	char	*tmp;
-	size_t	len;
-
-	if (tmp_char)
-	{
-		*line = ft_strndup(store, tmp_char - store);
-		len = ft_strlen(tmp_char + 1);
-		tmp = ft_strndup(tmp_char + 1, len);
-		free(store);
-		store = tmp;
-	}
-	if (store)
-	{
-		*line = store;
-		store = NULL;
-	}
-	else
-		*line = ft_strndup("", 1);
-	return (_EOF);
-}
-
 int		get_next_line(int fd, char **line)
 {
 	int			byte;
@@ -48,7 +25,7 @@ int		get_next_line(int fd, char **line)
 	}
 	if (byte < 0)
 		return (_ERROR);
-	return (ret_line(line, store, tmp_char));
+	return (0);
 }
 
 int		main(void)
