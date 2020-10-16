b23840a2859a0cb54f17a425482f19c844562df2
Wonil Park <wopark@c5r3s3.42seoul.kr>
2020-10-16 16:18:36 +0900

    2020-10-16 reset code

--- 02_Get_Next_Line/get_next_line.c
+++ 02_Get_Next_Line/get_next_line.c
@@ -4,28 +4,7 @@
 
 int		get_next_line(int fd, char **line)
 {
-	int			byte;
-	char		*tmp_char;
-	char		*tmp_store;
-	char		buf[BUFFER_SIZE + 1];
-	static char	*store;
 
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
-		return (_ERROR);
-	while (!(tmp_char = ft_strchr(store, '\n')) && (byte = read(fd, buf, BUFFER_SIZE)) > 0)
-	{
-		buf[byte] = 0;
-		if (!store)
-			tmp_store = ft_strndup(buf, byte);
-		else
-			tmp_store = ft_strjoin(store, buf);
-		if (store)
-			free(store);
-		store = tmp_store;
-	}
-	if (byte < 0)
-		return (_ERROR);
-	return (0);
 }
 
 int		main(void)
