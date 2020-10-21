902d1190366181acda135db50071115ddf037a23
Wonil Park <wopark@c6r2s4.42seoul.kr>
2020-10-21 21:44:37 +0900

    2020-10-21 printf

--- 03_Printf/ft_printf.c
+++ 03_Printf/ft_printf.c
@@ -1,6 +1,24 @@
 #include "ft_printf.h"
 
-int	ft_printf(const char *s, ...)
-{
 
+int	printf_parer(const char *format, va_list ap)
+{
+	while (*format)
+	{
+		if (*format == '%')
+		{
+
+		}
+	}
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		ret;
+
+	va_start(ap, format);
+	ret = printf_parser(format, ap);
+	va_end(ap);
+	return (ret);
 }
