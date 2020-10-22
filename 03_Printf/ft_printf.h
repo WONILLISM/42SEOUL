68732164f97057dd59cc08a2ff5cd83c27b333b9
Wonil Park <wopark@c5r3s7.42seoul.kr>
2020-10-22 21:27:10 +0900

    2020-10-22 printf parser

--- 03_Printf/ft_printf.h
+++ 03_Printf/ft_printf.h
@@ -1,11 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wopark <wopark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/22 21:23:24 by wopark            #+#    #+#             */
+/*   Updated: 2020/10/22 21:24:28 by wopark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <stdarg.h>
+# include <unistd.h>
 # include "./libft/libft.h"
 
 typedef	struct	s_info
 {
-	int		byte;
+	int		n;
 	int		minus;
 	int		width;
 	int		precision;
@@ -17,4 +30,5 @@ typedef	struct	s_info
 }				t_info;
 
 int	ft_printf(const char *, ...);
+
 #endif
