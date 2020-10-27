7205d929b11f2860fa46c4a5f8ff46fd69692c7d
Wonil Park <wopark@c5r6s3.42seoul.kr>
2020-10-27 13:26:57 +0900

    2020-10-27 gnl passed

--- 02_Get_Next_Line/get_next_line_bonus.c
+++ 02_Get_Next_Line/get_next_line_bonus.c
@@ -38,14 +38,13 @@ int			get_next_line(int fd, char **line)
 	(byte = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[byte] = 0;
-		if (store[fd])
-			tmp_ptr = ft_strjoin(store[fd], buf);
-		else
-			tmp_ptr = ft_strndup(buf, byte);
+		tmp_ptr = store[fd] != NULL ?
+		ft_strjoin(store[fd], buf) : ft_strndup(buf, byte);
 		if (store[fd])
 			free(store[fd]);
 		store[fd] = tmp_ptr;
 	}
+	free(buf);
 	if (byte < 0)
 		return (_ERROR);
 	return (proc_remain(line, &store[fd], tmp_ptr));
