a31fd7159c348301a2f2a02b4fb63fbe4186a53b
WONILLISM <wonillism@gmail.com>
2020-11-09 11:53:52 +0900

    2020-11-09

--- 03_Printf/main.c
+++ 03_Printf/main.c
@@ -4,6 +4,12 @@ int		main(void)
 {
 	printf("[%#10x]\n", 15);
 	ft_printf("[%#10x]\n", 15);
+
+/*
+hex 너비 음수인경우 처리
+*/
+	printf("-->|%#-4.X|<--\n", 15);
+	ft_printf("-->|%#-4.X|<--\n", 15);
 	return (0);
 }
 
