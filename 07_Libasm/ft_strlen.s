5d1af16fd86f451185d001e94846dfa98e4e06a4
Wonil Park <wopark@c6r7s4.42seoul.kr>
2021-04-19 14:02:10 +0900

    2021-04-19

--- 07_Libasm/ft_strlen.s
+++ 07_Libasm/ft_strlen.s
@@ -12,13 +12,13 @@ section	.text
 		global	_ft_strlen
 
 _ft_strlen:
-		mov	rax, 0	; 리턴값이 s의 인덱스 이므로  rax 레지스터를 인덱스로 사용, 0으로 초기화
-		jmp	count	; count lable로 점프
+	mov	rax, 0	; 리턴값이 s의 인덱스 이므로 rax 레지스터를 인덱스로 사용, 0으로 초기화
+	jmp	count	; count lable로 점프
 count:
-		cmp BYTE [rdi + rax], 0
-		je	end		; 비교연산 결과 두 인자가 같으면 end label로 점프, 아니면 무시
-		inc	rax		; NULL이 아니라면 인덱스 +1
-		jmp	count	; count label 반복
+	cmp BYTE [rdi + rax], 0
+	je	end		; 비교연산 결과 두 인자가 같으면 end label로 점프, 아니면 무시
+	inc	rax		; NULL이 아니라면 인덱스 +1
+	jmp	count	; count label 반복
 end:
-		ret			; 함수 종료, rax 반환
+	ret			; 함수 종료, rax 반환
 
