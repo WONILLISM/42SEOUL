28750bb17b09dbb1afaf16b7905614ee0fb5d0c5
Wonil Park <wopark@c6r7s4.42seoul.kr>
2021-04-19 14:03:19 +0900

    2021-04-19 add ft_strcpy

--- 07_Libasm/main.c
+++ 07_Libasm/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int		ft_strlen (char *s);
+int		ft_strlen(char *s);
+char	*ft_strcpy(char *dst, char *src);
+int		ft_strcmp();
 int		main()
 {
-	printf("%d\n",ft_strlen("Hello"));
+	char	*src;
+	char	dst[10];
+
+	src = "Hello";
+	printf("***** ft_strlen *****\n[Length of \"Hello\"]: %d\n", ft_strlen(src));
+	src = "World";
+	printf("***** ft_strcpy *****\n[src: \"World\"] dst: %s \n", ft_strcpy(dst, src));
 	return (0);
 }
