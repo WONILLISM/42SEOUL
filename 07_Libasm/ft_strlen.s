1a6456679319cabd4acd9c9d68d12bc9f290dd8c
Wonil Park <wopark@c6r7s4.42seoul.kr>
2021-04-19 14:42:42 +0900

    2021-04-19 ft_strcmp.s

--- 07_Libasm/ft_strlen.s
+++ 07_Libasm/ft_strlen.s
@@ -15,7 +15,7 @@ _ft_strlen:
 	mov	rax, 0	; 리턴값이 s의 인덱스 이므로 rax 레지스터를 인덱스로 사용, 0으로 초기화
 	jmp	count	; count lable로 점프
 count:
-	cmp BYTE [rdi + rax], 0
+	cmp	BYTE [rdi + rax], 0
 	je	end		; 비교연산 결과 두 인자가 같으면 end label로 점프, 아니면 무시
 	inc	rax		; NULL이 아니라면 인덱스 +1
 	jmp	count	; count label 반복
