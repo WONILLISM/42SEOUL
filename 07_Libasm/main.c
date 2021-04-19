8f45bc9e9822e0e9cfc133e4616344f9943732f6
Wonil Park <wopark@c6r7s4.42seoul.kr>
2021-04-19 14:42:24 +0900

    2021-04-19 add ft_strcmp test

--- 07_Libasm/main.c
+++ 07_Libasm/main.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 int		ft_strlen(char *s);
 char	*ft_strcpy(char *dst, char *src);
-int		ft_strcmp();
+int		ft_strcmp(const char *s1, const char *s2);
 int		main()
 {
 	char	*src;
+	char	*src2;
 	char	dst[10];
 
+	/*    ft_strlen    */
 	src = "Hello";
-	printf("***** ft_strlen *****\n[Length of \"Hello\"]: %d\n", ft_strlen(src));
+	printf("\n***** ft_strlen *****\n");
+	printf("[Length of \"Hello\"]: %d\n", ft_strlen(src));
+
+	/*    ft_strcpy    */
 	src = "World";
-	printf("***** ft_strcpy *****\n[src: \"World\"] dst: %s \n", ft_strcpy(dst, src));
+	printf("\n***** ft_strcpy *****\n");
+	printf("[src: \"World\"] dst: %s \n", ft_strcpy(dst, src));
+
+	/*    ft_strcmp    */
+	src = "Hello";
+	src2 = "Hello";
+	printf("\n***** ft_strcmp equal*****\n");
+	printf("[src: \"Hello\" src2: \"Hello\"] : ");
+	if (ft_strcmp(src, src2))
+		printf("not equal\n");
+	else
+		printf("equal\n");
+
+	src = "Hello";
+	src2 = "World";
+	printf("\n***** ft_strcmp not equal*****\n");
+	printf("[src: \"Hello\" src2: \"World\"] : ");
+	if (ft_strcmp(src, src2))
+		printf("not equal\n");
+	else
+		printf("equal\n");
+
 	return (0);
 }
