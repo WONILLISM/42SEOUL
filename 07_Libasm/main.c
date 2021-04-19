4927a68a61f49751c326cbd9b5b5edfa20ac03d9
Wonil Park <wopark@c6r7s4.42seoul.kr>
2021-04-19 18:56:44 +0900

    2021-04-19 add ft_strdup

--- 07_Libasm/main.c
+++ 07_Libasm/main.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -6,11 +7,14 @@ int		ft_strlen(char *s);
 char	*ft_strcpy(char *dst, char *src);
 int		ft_strcmp(const char *s1, const char *s2);
 ssize_t	ft_write(int fd, const void *buf, size_t nbyte);
+ssize_t	ft_read(int fd, const void *buf, size_t nbyte);
+char	*ft_strdup(const char *src);
 
 int		main(int argc, char **argv)
 {
 	char	*src;
 	char	*src2;
+	char	src3[20];
 	char	dst[10];
 
 	/*    ft_strlen    */
@@ -69,7 +73,24 @@ int		main(int argc, char **argv)
 		printf("[argv[1]: %s ft_write res:]\n", argv[1]);
 		write(1, argv[1], ft_strlen(argv[1]));
 		write(1, "\n", 1);
+
+		printf("\n***** ft_read *****\n");
+		printf("[ft_read to src]\n");
+		ft_read(0, src3, 20);
+		printf("src : %s\n", src3);
+
+		printf("\n***** read *****\n");
+		printf("[read to src]\n");
+		read(0, src3, 20);
+		printf("src : %s\n", src3);
 	}
+
+	/*    ft_strcpy    */
+	src = "World";
+	printf("\n***** ft_strdup *****\n");
+	printf("[src: \"World\"] dst: %s \n", ft_strdup(src));
+	printf("\n***** strdup *****\n");
+	printf("[src: \"World\"] dst: %s \n", strdup(src));
 
 	return (0);
 }
