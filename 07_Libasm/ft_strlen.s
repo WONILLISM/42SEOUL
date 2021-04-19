aa7d0c893606bb6a02c8b782ef7224939d24203d
wopark <wopark@yujin-ui-MacBookAir.local>
2021-04-19 10:57:44 +0900

    2021-04-19

--- 07_Libasm/ft_strlen.s
+++ 07_Libasm/ft_strlen.s
@@ -10,13 +10,15 @@
 
 section	.text
 		global	_ft_strlen
+
 _ft_strlen:
-		mov	rax, 0
-		jmp	count
+		mov	rax, 0	; 리턴값이 s의 인덱스 이므로  rax 레지스터를 인덱스로 사용, 0으로 초기화
+		jmp	count	; count lable로 점프
 count:
 		cmp BYTE [rdi + rax], 0
-		je	end
-		inc	rax
-		jmp	count
+		je	end		; 비교연산 결과 두 인자가 같으면 end label로 점프, 아니면 무시
+		inc	rax		; NULL이 아니라면 인덱스 +1
+		jmp	count	; count label 반복
 end:
-		ret
+		ret			; 함수 종료, rax 반환
+
