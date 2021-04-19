3f8b6b385909642b03c50059a78ee878eeccfd31
Wonil Park <wopark@c6r7s4.42seoul.kr>
2021-04-19 17:43:34 +0900

    2021-04-19 add comment

--- 07_Libasm/ft_write.s
+++ 07_Libasm/ft_write.s
@@ -1,17 +1,17 @@
 section	.text
 	global	_ft_write
-	extern	___error	; ___error 함수 호출
+	extern	___error	; 외부 함수인 ___error 함수 호출하기 위함
 
 _ft_write:
-	mov	rax, 0x02000004	;
-	syscall
-	jc	_err
+	mov	rax, 0x2000004	; sys_call numver 4 = sys_write 를 rax에 대입
+	syscall	; syscall
+	jc	_err	; carry flag가 1이면 _err 로 점프
 	ret
 
 _err:
-	push	rax
-	call	___error
-	pop		rdx
+	push	rax			; syscall 함수를 호출한 결과의 에러코드인 rax 값을 스택에 push
+	call	___error	; __error 함수 호출
+	pop		rdx			;
 	mov		[rax], rdx
 	mov		rax, -1
 	ret
