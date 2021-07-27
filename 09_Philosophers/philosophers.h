a6060959ad2d0ec1aa4ff3d616d40abb4f322492
WONILLISM <47653005+WONILLISM@users.noreply.github.com>
2021-07-27 19:00:18 +0900

    norm check

--- 09_Philosophers/philosophers.h
+++ 09_Philosophers/philosophers.h
@@ -33,11 +33,11 @@ typedef struct s_philo
 	long			last_eat_time;
 }					t_philo;
 
-int	ft_isdigit(int c);
-int	ft_atoi(const char *str);
-int	init_thread(t_data *data);
-int	init_data(t_data *data, int argc, char **argv);
-int	init_philo(t_data *data, t_philo **philo);
+int		ft_isdigit(int c);
+int		ft_atoi(const char *str);
+int		init_thread(t_data *data);
+int		init_data(t_data *data, int argc, char **argv);
+int		init_philo(t_data *data, t_philo **philo);
 long	microtomilli(void);
 void	ft_sleep(long t, long cur);
 void	pick_fork(t_philo *philo);
