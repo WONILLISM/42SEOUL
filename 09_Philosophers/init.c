367ef8ecdd594d263da394fc675e96ee0d639c10
Wonil Park <wopark@c1r5s7.42seoul.kr>
2021-07-27 17:31:56 +0900

    norm check ok

--- 09_Philosophers/init.c
+++ 09_Philosophers/init.c
@@ -55,12 +55,12 @@ int	init_philo(t_data *data, t_philo **philo)
 	{
 		(*philo)[i].idx = i + 1;
 		(*philo)[i].eat_cnt = 0;
-		(*philo)[i].left_fork_idx =
+		(*philo)[i].left_fork_idx = \
 			(data->num_of_philos + i - 1) % data->num_of_philos + 1;
 		if ((*philo)[i].idx == data->num_of_philos)
 			(*philo)[i].right_fork_idx = data->num_of_philos;
 		else
-			(*philo)[i].right_fork_idx =
+			(*philo)[i].right_fork_idx = \
 				(data->num_of_philos + i + 1) % data->num_of_philos;
 		(*philo)[i].status = 0;
 		(*philo)[i].last_eat_time = microtomilli();
