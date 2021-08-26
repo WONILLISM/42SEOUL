f03113d5e915cbc6360de7bc4a7c89e4a389a2fa
WONILLISM <wonillism@gmail.com>
2021-08-26 22:15:43 +0900

    complete ex05

---
+++ CPP_Module_01/ex05/Karen.hpp
@@ -0,0 +1,16 @@
+#ifndef KAREN_HPP
+# define KAREN_HPP
+# include <iostream>
+
+class Karen
+{
+private:
+	void	debug(void);
+	void	info(void);
+	void	warning(void);
+	void	error(void);
+	void	nothing(void);
+public:
+	void	complain(std::string level);
+};
+#endif
