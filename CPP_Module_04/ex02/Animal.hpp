dfebb09da7b9302a001e8f3308db49efb9a3dcfe
WONILLISM <wonillism@gmail.com>
2021-09-25 18:48:22 +0900

    modify

--- CPP_Module_04/ex02/Animal.hpp
+++ CPP_Module_04/ex02/Animal.hpp
@@ -12,6 +12,10 @@ public:
 	Animal& operator=(const Animal& copy);
 	virtual ~Animal();
 	std::string const	&getType() const;
+/*
+** 추상클래스는 함수를 재정의하지 않으면 코드상에서 오류로 판단한다.
+** 일반적인 가상함수도 재정의가 가능하지만 무조건적이지 않아 실수를 할 수 있다.
+*/
 	virtual void		makeSound() const = 0;
 };
 
